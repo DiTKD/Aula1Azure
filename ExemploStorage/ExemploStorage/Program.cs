ae003a3cf8ea0002c00f659b18bb2255cea799a6
logonrmlocal <logonrmlocal@L2201MICRO36>
2018-12-03 22:28:40 -0200

    Aula 2

--- ExemploStorage/ExemploStorage/Program.cs
+++ ExemploStorage/ExemploStorage/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
 using Microsoft.WindowsAzure.Storage.Table;
+using StackExchange.Redis;
 
 namespace ExemploStorage
 {
@@ -9,27 +10,15 @@ namespace ExemploStorage
     {
         static void Main(string[] args)
         {
-            //CloudStorageAccount account = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=storage13net;AccountKey=AhDlxuw3BZ3XhhFvgJUNmvVDBoiUZFXwrZoty/hLCiXdFzR2B/wXVnzlbS/dnRIt7oC/jn8mcvNYoaDluvercg==;EndpointSuffix=core.windows.net");
-            CloudStorageAccount account = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=storagediogo13;AccountKey=SBrsPwCIUU8Oauhb9AvE8K1Fc4qsj8J5pFOHZABaxo9zVcD+VQLVwN7eqovA8RTzrzHejpEw5zBLqHr9JxeMVQ==;EndpointSuffix=core.windows.net");
-            var tableClient = account.CreateCloudTableClient();
-            var table = tableClient.GetTableReference("rm331835");
-            table.CreateIfNotExistsAsync().Wait();
+            Queue();
 
-            var entity = new Aluno("33183531", "Taboão da Serra");
-            entity.Nome = "Diogo";
-            entity.Email = "DiogoCrisostomos@gmail.com";
-
-
-            table.ExecuteAsync(TableOperation.Insert(entity));
-
-            Console.WriteLine($"{table.Uri}");
 
             Console.Read();
 
             Console.WriteLine("Hello World!");
         }
 
-        public void CriarBlob()
+        public static void CriarBlob()
         {
             //CloudStorageAccount account = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=storage13net;AccountKey=AhDlxuw3BZ3XhhFvgJUNmvVDBoiUZFXwrZoty/hLCiXdFzR2B/wXVnzlbS/dnRIt7oC/jn8mcvNYoaDluvercg==;EndpointSuffix=core.windows.net");
             CloudStorageAccount account = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=storagediogo13;AccountKey=SBrsPwCIUU8Oauhb9AvE8K1Fc4qsj8J5pFOHZABaxo9zVcD+VQLVwN7eqovA8RTzrzHejpEw5zBLqHr9JxeMVQ==;EndpointSuffix=core.windows.net");
@@ -50,6 +39,90 @@ namespace ExemploStorage
             Console.Read();
 
             Console.WriteLine("Hello World!");
+        }
+
+        public static void CriarTable()
+        {
+            //CloudStorageAccount account = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=storage13net;AccountKey=AhDlxuw3BZ3XhhFvgJUNmvVDBoiUZFXwrZoty/hLCiXdFzR2B/wXVnzlbS/dnRIt7oC/jn8mcvNYoaDluvercg==;EndpointSuffix=core.windows.net");
+            CloudStorageAccount account = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=storagediogo13;AccountKey=SBrsPwCIUU8Oauhb9AvE8K1Fc4qsj8J5pFOHZABaxo9zVcD+VQLVwN7eqovA8RTzrzHejpEw5zBLqHr9JxeMVQ==;EndpointSuffix=core.windows.net");
+            var tableClient = account.CreateCloudTableClient();
+            var table = tableClient.GetTableReference("rm331835");
+            table.CreateIfNotExistsAsync().Wait();
+
+            var entity = new Aluno("33183531", "Taboão da Serra");
+            entity.Nome = "Diogo";
+            entity.Email = "DiogoCrisostomos@gmail.com";
+
+
+            table.ExecuteAsync(TableOperation.Insert(entity));
+
+            Console.WriteLine($"{table.Uri}");
+        }
+
+        public static void Redis()
+        {
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("trezenetredis.redis.cache.windows.net:6380,password=ftXkXFGfHf9Pf4sIJ8LVIgSQTAmU7I38nnRf96qmipE=,ssl=True,abortConnect=False");
+            var db = redis.GetDatabase();
+
+            Console.WriteLine("Digite Seu RM");
+            var rm = Console.ReadLine();
+
+            Console.WriteLine("Digite Seu Nome");
+            var nome = Console.ReadLine();
+
+
+            db.StringSet(rm, nome);
+
+            Console.WriteLine($"chave: {rm}, valor: {db.StringGet(rm)}");
+
+            Console.Read();
+        }
+
+        public static void Chart()
+        {
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("trezenetredis.redis.cache.windows.net:6380,password=ftXkXFGfHf9Pf4sIJ8LVIgSQTAmU7I38nnRf96qmipE=,ssl=True,abortConnect=False");
+            var db = redis.GetDatabase();
+
+            Console.WriteLine("Digite Seu Nome");
+            var nome = Console.ReadLine();
+            db.Publish("13net", $"{nome} entrou na sala");
+
+            var pubsub = redis.GetSubscriber();
+            pubsub.Subscribe("13net", (chanel, message) => 
+                Console.WriteLine(message.ToString()));
+            while(true)
+            {
+                var msg = Console.ReadLine();
+                db.Publish("13net", msg);
+            }
+
+            
+            Console.Read();
+        }
+
+        public static void Queue()
+        {
+            var connection = "DefaultEndpointsProtocol=https;AccountName=trezenet;AccountKey=AXwUsljgM169Q3c9IvcunCdagOXypuVtbaSs/mMmPCrPuMADu9rW7BYeEAE/B5Qm5v9sM956wpbBjpJYoj/8UQ==;EndpointSuffix=core.windows.net";
+
+            CloudStorageAccount account = CloudStorageAccount.Parse(connection);
+            var queueClient = account.CreateCloudQueueClient();
+            var queue = queueClient.GetQueueReference("rm331835_");
+            queue.CreateIfNotExistsAsync().Wait();
+
+            Console.WriteLine("Digite algo");
+            var msg = Console.ReadLine();
+
+            queue.AddMessageAsync(new Microsoft.WindowsAzure.Storage.Queue.CloudQueueMessage(msg), new TimeSpan(1000), null, null, null);
+
+
+            var messages = queue.GetMessagesAsync(10).Result;
+            foreach (var m in messages)
+            {
+                Console.WriteLine($"Mensagem: {m.AsString}");
+                queue.DeleteMessageAsync(m);
+            }
+
+            Console.ReadLine();
         }
     }
 }
